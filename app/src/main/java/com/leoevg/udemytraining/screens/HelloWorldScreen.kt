2537f0df4e76a0521b9682fe5efd6cb9da0c744d
ShemLeon <shemiakinleonid@gmail.com>
2025-03-31 15:29:14 +0300

    1 - Hello World

--- app/src/main/java/com/leoevg/udemytraining/screens/HelloWorldScreen.kt
+++ app/src/main/java/com/leoevg/udemytraining/screens/HelloWorldScreen.kt
@@ -9,25 +9,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.leoevg.udemytraining.navigation.MainNavHost
+import com.leoevg.udemytraining.ui.theme.UdemyTrainingTheme
 
 @Composable
-fun HelloWorld(
-
-){
+fun HelloWorld() {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-
         ) {
         Text(
             "Hello world!",
             fontSize = 40.sp,
-            color = Color.Blue,
-            modifier = Modifier.padding(top = 40.dp)
+            color = Color.Blue
         )
     }
-    
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HelloWorldPreview() {
+    UdemyTrainingTheme {
+        HelloWorld()
+    }
 }
