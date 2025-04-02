e6602d8a08e02fc375b0f803a3396ca44faf5dc3
ShemLeon <shemiakinleonid@gmail.com>
2025-04-02 15:26:42 +0300

    DaggerHilt

--- app/src/main/java/com/leoevg/udemytraining/di/BasicModule.kt
+++ app/src/main/java/com/leoevg/udemytraining/di/BasicModule.kt
@@ -1,16 +1,31 @@
 package com.leoevg.udemytraining.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-// это какая-то структура, в которой я обьявлю для Dagger Hilt
-// Как правило это функции-"провайдеры", которые умеют билдить объекты
-// @InstallIn - аннотация, чтобы DH понял, куда сохранить объект
-
 @Module
 @InstallIn(SingletonComponent::class)
 object BasicModule {
-
+    @Provides
+    @Singleton
+    fun providerSHWManager(): SayHelloWorldManager{
+        return SayHelloWorldManager("AppUser")
+    }
 }
+
+
+// практическое применение - в какой то момент в приложении может быть запрошен
+// объект типа SayHelloWorldManager я использую функцию providerSHWManager -
+// чтобы получить из нее этот объект и его туда запушить
+
+// Это структура, в которой я обьявлю для Dagger Hilt
+// функции-"провайдер", которые умеют билдить объекты
+// @InstallIn - аннотация, чтобы DH понял, куда сохранить объект
+// Singleton - говорит о том, что этот модуль будет создан в начале приложения
+// и сохранен на весь участок работы приложухи. можно привязать только к активности
+
+// ::  - это ссылка на класс
+
