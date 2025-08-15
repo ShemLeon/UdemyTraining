dcfa6c6cffa16c45d7afe795bf91980ff2176aee
ShemLeon <shemiakinleonid@gmail.com>
2025-08-16 02:45:35 +0300

    14 - Context Shared preferences

--- app/src/main/java/com/leoevg/udemytraining/screens/_14_UseCase/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/leoevg/udemytraining/screens/_14_UseCase/data/repository/UserRepositoryImpl.kt
@@ -7,20 +7,25 @@ import com.leoevg.udemytraining.screens._14_UseCase.domain.repository.UserReposi
 
 private const val SHARED_PREFS_NAME = "shared_prefs_name"
 private const val KEY_FIRST_NAME = "firstName"
+private const val KEY_LAST_NAME = "lastName"
+private const val DEFAULT_NAME = "Default last name"
 
-class UserRepositoryImpl(private val context: Context): UserRepository {
-    val sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+class UserRepositoryImpl(private val context: Context) : UserRepository {
+    private val sharedPreferences =
+        context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
 
     override fun saveName(saveParam: SaveUserNameParam): Boolean {
-        sharedPreferences.edit().putString(KEY_NAME, saveParam.name).apply()
-
+        sharedPreferences.edit().putString(KEY_FIRST_NAME, saveParam.name).apply()
         return true
     }
 
     override fun getName(): UserName {
+        val firstName = sharedPreferences.getString(KEY_FIRST_NAME, "") ?: ""
+        val lastName = sharedPreferences.getString(KEY_LAST_NAME, DEFAULT_NAME) ?: DEFAULT_NAME
+
         return UserName(
-            firstName = "Leo",
-            lastName = "Hello from Repository"
+            firstName = firstName,
+            lastName = lastName
         )
     }
 }
